c91d72452a7454c03b162e52db62b18283ee024f
Kelvin Cortez <kelvcortez@gmail.com>
2018-10-30 09:53:25 +1300

    First Commit, New data project layer

---
+++ BibleForumSolution/BibleForum.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BibleForum.Data.Models;
+
+namespace BibleForum.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
---
+++ BibleForumSolution/BibleForum.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BibleForum.Data
+{
+    public class Class1
+    {
+    }
+}
