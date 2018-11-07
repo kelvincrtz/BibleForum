6ca0d9d88b9d40f8755fab78f3a663649b119475
Kelvin Cortez <kelvcortez@gmail.com>
2018-11-08 10:04:01 +1300

    Style Change Experiment, Landing Page

--- BibleForumSolution/BibleForum.Service/PostService.cs
+++ BibleForumSolution/BibleForum.Service/PostService.cs
@@ -55,9 +55,8 @@ namespace BibleForum.Service
 
         public IEnumerable<Post> GetFilteredPost(Forum forum, string searchQuery)
         {
-            return 
-                string.IsNullOrEmpty(searchQuery) ? 
-                forum.Posts : forum.Posts.Where(post => post.Title.Contains(searchQuery) || post.Content.Contains(searchQuery));
+            return string.IsNullOrEmpty(searchQuery) ? forum.Posts 
+                : forum.Posts.Where(post => post.Title.Contains(searchQuery) || post.Content.Contains(searchQuery));
         }
 
         public IEnumerable<Post> GetLastestPosts(int n)
