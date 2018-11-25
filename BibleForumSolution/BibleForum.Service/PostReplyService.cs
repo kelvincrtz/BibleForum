162b2f72c6d7c0ef148aae08aee40460acfdb4d4
Kelvin Cortez <kelvcortez@gmail.com>
2018-11-26 11:49:22 +1300

    PostService Created, GetById Implemented

---
+++ BibleForumSolution/BibleForum.Service/PostReplyService.cs
@@ -0,0 +1,43 @@
+﻿using BibleForum.Data;
+using BibleForum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleForum.Service
+{
+    public class PostReplyService : IPostReply
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public PostReplyService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditPostReplyContent(int id, string newContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PostReply> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PostReply GetById(int id)
+        {
+            return _dbContext.PostReplies.Where(postReply => postReply.Id == id)
+                .Include(u => u.User)
+                .FirstOrDefault();
+        }
+    }
+}
