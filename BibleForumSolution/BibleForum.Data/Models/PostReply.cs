30cbb71a89ffbc09c8ee5ed615dbc702cf03c8cc
Kelvin Cortez <kelvcortez@gmail.com>
2018-11-27 11:22:59 +1300

    Added New properties for PostReply class

--- BibleForumSolution/BibleForum.Data/Models/PostReply.cs
+++ BibleForumSolution/BibleForum.Data/Models/PostReply.cs
@@ -9,6 +9,9 @@ namespace BibleForum.Data.Models
         public int Id { get; set; }
         public string Content { get; set; }
         public DateTime Created { get; set; }
+        public DateTime EditedDate { get; set; }
+        public bool IsEdited { get; set; }
+        public int VoteCount { get; set; }
 
         public virtual ApplicationUser User { get; set; }
         public virtual Post Post { get; set; }
