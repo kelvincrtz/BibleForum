963b3bde1754a811eb5a0d42674bd0f8cbe9c471
Kelvin Cortez <kelvcortez@gmail.com>
2018-11-27 11:29:14 +1300

    Database Migrations for New properties for Post Reply

--- BibleForumSolution/BibleForum.Data/Migrations/20181126222413_New Properties for Post Reply.Designer.cs
+++ BibleForumSolution/BibleForum.Data/Migrations/20181126222413_New Properties for Post Reply.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BibleForum.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181126222413_New Properties for Post Reply")]
+    partial class NewPropertiesforPostReply
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BibleForumSolution/BibleForum.Data/Migrations/20181126222413_New Properties for Post Reply.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BibleForum.Data.Migrations
+{
+    public partial class NewPropertiesforPostReply : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EditedDate",
+                table: "PostReplies",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEdited",
+                table: "PostReplies",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VoteCount",
+                table: "PostReplies",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EditedDate",
+                table: "PostReplies");
+
+            migrationBuilder.DropColumn(
+                name: "IsEdited",
+                table: "PostReplies");
+
+            migrationBuilder.DropColumn(
+                name: "VoteCount",
+                table: "PostReplies");
+        }
+    }
+}
