34da286da2e7747a7705fd58e85a1d9d4a2386a8
Kelvin Cortez <kelvcortez@gmail.com>
2018-11-28 10:42:55 +1300

    Created a Controller with Test Naming

---
+++ BibleForumSolution/BibleForum/Controllers/SupportVerseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibleForum.Controllers
+{
+    public class SupportVerseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(int id)
+        {
+
+        }
+    }
+}
