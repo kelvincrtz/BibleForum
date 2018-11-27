9f1bacbb6a86ad04de8af8e86234b06b8e4ff076
Kelvin Cortez <kelvcortez@gmail.com>
2018-11-27 14:27:27 +1300

    Interface Implemented

---
+++ BibleForumSolution/BibleForum.Data/IPostReplySupportingBibleVerse.cs
@@ -0,0 +1,18 @@
+﻿using BibleForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleForum.Data
+{
+    public interface IPostReplySupportingBibleVerse
+    {
+        PostReplySupportingBibleVerse GetById(int id);
+        IEnumerable<PostReplySupportingBibleVerse> GetAll();
+
+        Task Add(int id);
+        Task Delete(int id);
+        Task EditPostReplyVerseContent(int id, string newContent, string newChapter, string newTranslation);
+    }
+}
