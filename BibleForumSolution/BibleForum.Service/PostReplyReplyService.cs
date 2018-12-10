98772bcbbcc8a2f0a4a54528317fda8588b8780b
Kelvin Cortez <kelvcortez@gmail.com>
2018-12-11 11:47:49 +1300

    Created new Service folder for ReplyReply

---
+++ BibleForumSolution/BibleForum.Service/PostReplyReplyService.cs
@@ -0,0 +1,39 @@
+﻿using BibleForum.Data;
+using BibleForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleForum.Service
+{
+    public class PostReplyReplyService : IPostReplyReply
+    {
+
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditPostReplyContent(int id, string newContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PostReplyReply> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PostReplyReply GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Vote(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
