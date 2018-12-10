b2558aeb61eadf9cbbc678e779add29af966a70c
Kelvin Cortez <kelvcortez@gmail.com>
2018-12-11 11:46:16 +1300

    IPostReplyReply Interface Methods

--- BibleForumSolution/BibleForum.Data/IPostReplyReply.cs
+++ BibleForumSolution/BibleForum.Data/IPostReplyReply.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using BibleForum.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BibleForum.Data
 {
     public interface IPostReplyReply
     {
+        PostReplyReply GetById(int id);
 
+        IEnumerable<PostReplyReply> GetAll();
+
+        Task Delete(int id);
+        Task EditPostReplyContent(int id, string newContent);
+
+        Task Vote(int id);
     }
 }
