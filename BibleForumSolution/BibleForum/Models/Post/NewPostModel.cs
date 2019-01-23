13b33a1264501abcb99b98c1657602f669bd8b20
Kelvin Cortez <kelvcortez@gmail.com>
2019-01-24 11:33:11 +1300

    Back-end: Added Requirements for NewPostModel

--- BibleForumSolution/BibleForum/Models/Post/NewPostModel.cs
+++ BibleForumSolution/BibleForum/Models/Post/NewPostModel.cs
@@ -14,10 +14,12 @@ namespace BibleForum.Models.Post
         public string ForumImageUrl { get; set; }
 
         [Required]
-        [StringLength(100)]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Title { get; set; }
 
         [Required]
+        [MinLength(3)]
         public string Content { get; set; }
 
         public DateTime Created { get; set; }
