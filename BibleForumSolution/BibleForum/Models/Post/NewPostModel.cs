4a6ab9d67e7d264e39515e69f3fb8676c564699e
Kelvin Cortez <kelvcortez@gmail.com>
2019-01-24 11:54:19 +1300

    Back-end and Front-end: NewPost Create Validation SUCCESS

--- BibleForumSolution/BibleForum/Models/Post/NewPostModel.cs
+++ BibleForumSolution/BibleForum/Models/Post/NewPostModel.cs
@@ -20,7 +20,7 @@ namespace BibleForum.Models.Post
         public string Title { get; set; }
 
         [Required]
-        [StringLength(10000, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(50000, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.MultilineText)]
         [Display(Name = "Content")]
         public string Content { get; set; }
