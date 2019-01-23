08b512ea6a8e3902aa0085d00cf3354d80d25860
Kelvin Cortez <kelvcortez@gmail.com>
2019-01-24 12:30:40 +1300

    Back-end: Added Data Annotation to PostReplyEditModel

--- BibleForumSolution/BibleForum/Models/Reply/PostReplyEditModel.cs
+++ BibleForumSolution/BibleForum/Models/Reply/PostReplyEditModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +22,10 @@ namespace BibleForum.Models.Reply
         public bool IsEdited { get; set; }
         public int VoteCount { get; set; }
 
+        [Required]
+        [StringLength(50000, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Content")]
         public string ReplyContent { get; set; }
 
         public int PostId { get; set; }
