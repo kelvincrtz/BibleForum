8841955ed191778b6f409a5da73986d5fdd009f9
Kelvin Cortez <kelvcortez@gmail.com>
2019-01-31 13:07:12 +1300

    Back-end: New Migrationn to database - PostReplyLike

--- BibleForumSolution/BibleForum.Data/Migrations/20190131000621_Added new database called PostReplyLike.Designer.cs
+++ BibleForumSolution/BibleForum.Data/Migrations/20190131000621_Added new database called PostReplyLike.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BibleForum.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190131000621_Added new database called PostReplyLike")]
+    partial class AddednewdatabasecalledPostReplyLike
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BibleForumSolution/BibleForum.Data/Migrations/20190131000621_Added new database called PostReplyLike.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BibleForum.Data.Migrations
+{
+    public partial class AddednewdatabasecalledPostReplyLike : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PostReplyLikes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PostReplyId = table.Column<int>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostReplyLikes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostReplyLikes_PostReplies_PostReplyId",
+                        column: x => x.PostReplyId,
+                        principalTable: "PostReplies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PostReplyLikes_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostReplyLikes_PostReplyId",
+                table: "PostReplyLikes",
+                column: "PostReplyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostReplyLikes_UserId",
+                table: "PostReplyLikes",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostReplyLikes");
+        }
+    }
+}
