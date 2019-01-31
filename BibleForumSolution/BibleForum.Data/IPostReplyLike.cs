1f7456f80aed62781ecd0be9af808b4d57f10d54
Kelvin Cortez <kelvcortez@gmail.com>
2019-01-31 13:11:08 +1300

    Back-end: New Interface - IPostReplyLike

---
+++ BibleForumSolution/BibleForum.Data/IPostReplyLike.cs
@@ -0,0 +1,15 @@
+﻿using BibleForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibleForum.Data
+{
+    public interface IPostReplyLike
+    {
+        PostReplyLike GetById(int id);
+        IEnumerable<PostReplyLike> GetAll();
+
+        IEnumerable<PostReplyLike> GetByPostReplyId(int postReplyId);
+    }
+}
